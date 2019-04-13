d6048d17a67194c6f2c3fcf7de7afaf46076d21e
Johannes Ernstsen <ernstsen.johannes@gmail.com>
2019-04-13 10:30:15 +0200

    added lifetime for points

--- sonar_displayer.py
+++ sonar_displayer.py
@@ -1,11 +1,13 @@
 import math
+import time
 import tkinter as tk
+
 import serial
 
 
 class Point:
     """
-    Simple point class for pretty code
+    Simple point class
     """
 
     def __init__(self, x: int, y: int):
@@ -16,44 +18,50 @@ class Point:
         return "(" + str(self.x) + ", " + str(self.y) + ")"
 
 
-def limit(value: int, bound: int):
-    """
-    Simple helper-method for limiting a value to stay inside radar
-
-    :param value: value to be limited
-    :param bound: limit to be respected. Must be positive
-    :return: max(min(value, limit), -limit)
-    """
-    return max(min(value, bound), -bound)
-
-
 class Radar:
 
-    def __init__(self, com_port: str, baudrate: int):
-        self.serial = serial.Serial(com_port, baudrate)
+    def __init__(self, hardware_port: str, baudrate: int, point_lifetime: int):
+        """
+        Application for displaying distance information from a sonar or a radar on arduino or a similar device.
 
+        :param hardware_port: port to read input from. Usually COM or dev/tty
+        :param baudrate: baudrate for readings
+        :param point_lifetime: how long a point is displayed after detection
+        """
+        self.serial = serial.Serial(hardware_port, baudrate)
+        self.point_lifetime = point_lifetime
         root = tk.Tk()
         root.title('Radar')
         canvas = tk.Canvas(root, width=600, height=310)
         canvas.pack(expand=True, fill=tk.BOTH)
 
         self.canvas = canvas
-        self.draw(None)
+        self.points = []
+        self.draw()
         self.read_loop()
 
-    def draw(self, point):
+    def draw(self):
         """
         Method for drawing point onto canvas
 
-        :param point: point to draw
+        Points are read from field var points.
+        They are displayed for a fixed number of seconds, defined in class constructor
         """
         self.canvas.delete("all")
         self.draw_basis()
 
-        if point is not None:
+        new_points = []
+        now = time.time()
+
+        for (point, ts) in self.points:
             x = point.x + 300
             y = 300 - point.y
             self.canvas.create_oval(x - 5, y - 5, x + 5, y + 5, width=1, fill='red')
+
+            if now < (ts + self.point_lifetime):
+                new_points.append((point, ts))
+
+        self.points = new_points
         self.canvas.update()
 
     def draw_basis(self):
@@ -80,12 +88,15 @@ class Radar:
         while True:
             distance = int(self.serial.readline())
             angle = int(self.serial.readline())
+            print("distance=" + str(distance) + ", angle=" + str(angle))
 
             y = int(min(distance, 300) * math.cos(math.radians(angle)))
             x = int(distance * math.sin(math.radians(angle)))
 
-            self.draw(Point(x, y))
+            self.points.append((Point(x, y), time.time()))
+
+            self.draw()
 
 
 if __name__ == "__main__":
-    radar = Radar("COM3", 9600)
+    radar = Radar("COM3", 9600, 5)
